078afcdbe897630ecf5806d6abc83b3f91d5f34f
florent astier <florenttier@gmail.com>
2021-05-27 12:55:11 +0200

    models done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ApplicationRecord
+    before_destroy :ensure_no_children
+
+    validates :title, presence: true, uniqueness: true
+    validates :overview, presence: true
+
+    has_many :bookmarks
+
+    def ensure_no_children
+        unless self.bookmarks.empty?
+            raise (ActiveRecord::InvalidForeignKey)
+            raise ActiveRecord::Rollback
+        end
+    end
 end
